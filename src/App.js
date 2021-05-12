083fccbe73599f20a837f92d6e68a8e3d82ffe67
issamoh <issammohameddaoud@gmail.com>
2021-05-12 14:57:43 +0100

    fixed argon template issues

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+
+import HeaderArgon from "./components/Header.js"
+import Tables from "./components/Tables.js"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Tables></Tables>
     </div>
   );
 }
