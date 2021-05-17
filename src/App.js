6553d78278cae891d52c9589ab3c2ce7cd947c75
issamoh <issammohameddaoud@gmail.com>
2021-05-17 19:48:19 +0100

    affichage liste bornes + map

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 
 import HeaderArgon from "./components/Header.js"
 import Tables from "./components/Tables.js"
+import ListBornes from "./components/ListBornes.js"
 
 function App() {
   return (
     <div className="App">
-        <Tables></Tables>
+        <ListBornes bornes={null}></ListBornes>
     </div>
   );
 }
