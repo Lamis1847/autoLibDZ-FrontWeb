f6f353020783221ff631e4fad08f8b98c724c9e9
badido18 <badido18@live.fr>
2021-05-21 17:04:00 +0100

    error message added

--- src/Connexion.js
+++ src/Connexion.js
@@ -1,14 +1,13 @@
 import React, { useState } from 'react' ;
 import {api, setCookie} from "./scripts/Network.js" ;
 
-
-
-
-
 const Connexion = () => {
     
     const [email, setemail] = useState('')
+
     const [mdp, setmdp] = useState('')
+
+    const [errMsg, seterrMsg] = useState('')
     
     const onChangeHandler = event => {
         switch (event.target.name) {
@@ -38,35 +37,40 @@ const Connexion = () => {
                     window.location.href = "/dashboard"
                 }
             }
-        ).catch( err => console.log(err))
+        ).catch( err => {
+            console.log(err)
+            seterrMsg("Le nom d'utilisateur ou le mot de passe n'est pas valide")
+
+        })
     }   
 
-        return ( 
-        <div className = "LoginPage" >
-            <div className = "Main" >
-                <img src="Logo 3.png" alt=""/>
-                <h1>Se Connecter</h1>
-                <p>Veuillez remplir les champs suivants afin de vous connecter</p>
-                <input type="text" placeholder="Nom d'utilisateur ou e-mail" name="email"  value={email} onChange={onChangeHandler}></input>
-                <input type="password" placeholder="Mot de passe" name="mdp" value={mdp} onChange={onChangeHandler} ></input>
+    return ( 
+    <div className = "LoginPage" >
+        <div className = "Main" >
+            <img src="Logo 3.png" alt=""/>
+            <h1>Se Connecter</h1>
+            <p>Veuillez remplir les champs suivants afin de vous connecter</p>
+            <input type="text" placeholder="Nom d'utilisateur ou e-mail" name="email"  value={email} onChange={onChangeHandler}></input>
+            <input type="password" placeholder="Mot de passe" name="mdp" value={mdp} onChange={onChangeHandler} ></input>
 
-                <button onClick={Connex}>
-                     <span>Suivant</span> 
-                </button>
+            <button onClick={Connex}>
+                    <span>Suivant</span> 
+            </button>
+            <p style={{ color : 'red'}}>{errMsg}</p>
 
-            </div> 
-            <div className = "BlackBand" >
-                <h1 style ={{ marginTop : '60px'}}>Tour de contrôle</h1>
-                <img src="settings 1.png" alt=""/>
-                <p>Tableau de bord pour <br/> la gestion du système</p>
-                <img className="img2" src="Group 1.png" alt="" style ={{ marginTop : 'auto' , marginBottom : '20%' }}/>
-            </div> 
-            <div className = "ColoredBand" >
+        </div> 
+        <div className = "BlackBand" >
+            <h1 style ={{ marginTop : '60px'}}>Tour de contrôle</h1>
+            <img src="settings 1.png" alt=""/>
+            <p>Tableau de bord pour <br/> la gestion du système</p>
+            <img className="img2" src="Group 1.png" alt="" style ={{ marginTop : 'auto' , marginBottom : '20%' }}/>
+        </div> 
+        <div className = "ColoredBand" >
 
 
-            </div>
+        </div>
 
-        </div>)
+    </div>)
     
 }
 
