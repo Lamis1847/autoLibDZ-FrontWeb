cd357e9a90415a7001b8b4fa782f37fef731ebdf
dbouidaine <hd_bouidaine@esi.dz>
2021-06-28 15:39:46 +0100

    change baseUrl

--- src/components/Dashboard/DashboardView.js
+++ src/components/Dashboard/DashboardView.js
@@ -24,8 +24,8 @@ import StatsCard from './StatsCard';
 
 const DashboardView = () => {
 
-    //const api_url="https://autolib-dz.herokuapp.com/api";
-    const api_url="http://localhost:4000/api";
+    const api_url="https://autolib-dz.herokuapp.com/api";
+    //const api_url="http://localhost:4000/api";
 
     if (window.Chart) {
         parseOptions(Chart, chartOptions());
